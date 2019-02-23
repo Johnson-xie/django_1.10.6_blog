cbfb585c84a09c21c827d70c39fc6b4f9bd3ec3c
Johnson <johnson19921005@163.com>
2019-02-23 16:38:02 +0800

    database makemigrations and migrate

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+# from django.utils import timezone
 class Category(models.Model):
 
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
 
     title = models.CharField(max_length=50)
     body = models.TextField()
+    # TODO 时间自动修改
     create_time = models.DateTimeField()
     modified_time = models.DateTimeField()
 
@@ -18,6 +25,8 @@ class Post(models.Model):
     category = models.ForeignKey(Category)
     tags = models.ManyToManyField(Tag,blank=True)
     author = models.ForeignKey(User)  # django内置模块
+    def __str__(self):
+        return self.title
 
 # 查看具体sql语句
 # python manage.py sqlmigrate blog 0001
